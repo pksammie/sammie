72f3349bd3b3ef704e42f75180fe8202a11867ac
Sammie <samsonpamilerin959@gmail.com>
2026-05-11 00:29:51 +0200

    updated codes

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,23 +2,31 @@ auth.onAuthStateChanged((user) => {
     let ul = document.querySelector('.links-container');
     let ur = document.querySelector('.responsive-side-bar');
     
-    // Remove only existing dynamic links to prevent duplicates
-    document.querySelectorAll('.dynamic-auth-link').forEach(el => el.remove());
+    // 1. Clear only dynamic auth links to prevent doubling
+    document.querySelectorAll('.auth-link').forEach(el => el.remove());
+
+    let links = '';
 
     if (user) {
         user.getIdTokenResult(true).then(idTokenResult => {
-            let links = `
-                <li class="link-item dynamic-auth-link"><a href="/dashboard.html" class="link">Dashboard</a></li>
-                <li class="link-item dynamic-auth-link"><a href="#" onclick="logoutUser()" class="link">Logout</a></li>
+            links = `
+                <li class="link-item auth-link"><a href="/dashboard.html" class="link">Dashboard</a></li>
+                <li class="link-item auth-link"><a href="#" onclick="logoutUser()" class="link">Logout</a></li>
             `;
+
             if (idTokenResult.claims.admin) {
-                links += `<li class="link-item dynamic-auth-link"><a href="/admin-panel" class="link" style="color: red; font-weight: bold;">Admin</a></li>`;
+                links += `<li class="link-item auth-link"><a href="/admin-panel" class="link" style="color: red; font-weight: bold;">Admin</a></li>`;
             }
-            // Adds links WITHOUT deleting the menu button
-            ul.insertAdjacentHTML('beforeend', links);
-            if(ur) ur.innerHTML = links.replace(/link-item/g, 'links-item');
+
+            // ADD TO DESKTOP NAVBAR (Before the menu button)
+            ul.insertAdjacentHTML('afterbegin', links);
+            
+            // ADD TO MOBILE SIDEBAR
+            if(ur) ur.innerHTML += links.replace(/link-item/g, 'links-item');
         });
     } else {
-        ul.insertAdjacentHTML('beforeend', `<li class="link-item dynamic-auth-link"><a href="/dashboard.html" class="link">Login</a></li>`);
+        links = `<li class="link-item auth-link"><a href="/dashboard.html" class="link">Login</a></li>`;
+        ul.insertAdjacentHTML('afterbegin', links);
+        if(ur) ur.innerHTML += links.replace(/link-item/g, 'links-item');
     }
 });
