a46e42ffed203d92d1814d79339e4c599f6ea7b8
Sammie <samsonpamilerin959@gmail.com>
2026-05-11 01:25:10 +0200

    codes

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,26 +2,23 @@ auth.onAuthStateChanged((user) => {
     let ul = document.querySelector('.links-container');
     let ur = document.querySelector('.responsive-side-bar');
     
-    // 1. Remove only the old auth links to avoid doubling
-    document.querySelectorAll('.auth-link').forEach(el => el.remove());
+    // Remove only existing dynamic links to prevent duplicates
+    document.querySelectorAll('.dynamic-auth-link').forEach(el => el.remove());
 
-    let links = '';
     if (user) {
         user.getIdTokenResult(true).then(idTokenResult => {
-            links = `
-                <li class="link-item auth-link"><a href="/dashboard.html" class="link">Dashboard</a></li>
-                <li class="link-item auth-link"><a href="#" onclick="logoutUser()" class="link">Logout</a></li>
+            let links = `
+                <li class="link-item dynamic-auth-link"><a href="/dashboard.html" class="link">Dashboard</a></li>
+                <li class="link-item dynamic-auth-link"><a href="#" onclick="logoutUser()" class="link">Logout</a></li>
             `;
             if (idTokenResult.claims.admin) {
-                links += `<li class="link-item auth-link"><a href="/admin-panel" class="link" style="color: red; font-weight: bold;">Admin</a></li>`;
+                links += `<li class="link-item dynamic-auth-link"><a href="/admin-panel" class="link" style="color: red; font-weight: bold;">Admin</a></li>`;
             }
-            // 2. Add links BEFORE the menu button so the icon never moves
-            ul.insertAdjacentHTML('afterbegin', links);
-            if(ur) ur.innerHTML += links.replace(/link-item/g, 'links-item');
+            // Adds links WITHOUT deleting the menu button
+            ul.insertAdjacentHTML('beforeend', links);
+            if(ur) ur.innerHTML = links.replace(/link-item/g, 'links-item');
         });
     } else {
-        links = `<li class="link-item auth-link"><a href="/dashboard.html" class="link">Login</a></li>`;
-        ul.insertAdjacentHTML('afterbegin', links);
-        if(ur) ur.innerHTML += links.replace(/link-item/g, 'links-item');
+        ul.insertAdjacentHTML('beforeend', `<li class="link-item dynamic-auth-link"><a href="/dashboard.html" class="link">Login</a></li>`);
     }
 });
