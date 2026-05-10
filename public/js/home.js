1974eaa141954c1cd028174510867374a52b7bc2
Sammie <samsonpamilerin959@gmail.com>
2026-05-10 19:44:27 +0200

    updated codes

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,19 @@
 const blogSection = document.querySelector('.blogs-section');
 
-// Just get everything. No "if" check needed on the main homepage.
+// 1. Get the current blog ID from the URL (if any)
+let currentBlogId = location.pathname.split("/").filter(x => x && x !== 'editor').pop();
+
 db.collection("blogs").get().then((blogs) => {
     blogs.forEach(blog => {
-        createBlog(blog);
+        // 2. ONLY show the blog if it's NOT the one we are currently reading
+        if(blog.id !== currentBlogId) {
+            createBlog(blog);
+        }
     });
 }).catch(err => console.log("Error loading blogs:", err));
 
 const createBlog = (blog) => {
     let data = blog.data();
-    // We use / before blog.id to ensure the link works from any page
     blogSection.innerHTML += `
     <div class="blog-card">
         <img src="${data.bannerImage}" class="blog-image" alt="">
@@ -22,17 +26,17 @@ const createBlog = (blog) => {
     </div>
     `;
 }
-// This listens for changes in your Firebase 'announcements' collection
+
+// Global Alert Listener
 db.collection("announcements").doc("current").onSnapshot(doc => {
     const data = doc.data();
     const bar = document.getElementById('global-alert');
     const msg = document.getElementById('alert-msg');
     
-    if (data && data.active) {
+    if (data && data.active && msg && bar) {
         bar.style.display = 'block';
         msg.innerText = data.text;
-    } else {
+    } else if (bar) {
         bar.style.display = 'none';
     }
 });
-
