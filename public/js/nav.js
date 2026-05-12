f41822402419058c26a95f9c2b19ece400477541
Sammie <samsonpamilerin959@gmail.com>
2026-05-12 08:46:57 +0100

    updated codes

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,23 +2,33 @@ auth.onAuthStateChanged((user) => {
     let ul = document.querySelector('.links-container');
     let ur = document.querySelector('.responsive-side-bar');
     
-    // Remove only existing dynamic links to prevent duplicates
+    // Remove old dynamic links so they don't stack up
     document.querySelectorAll('.dynamic-auth-link').forEach(el => el.remove());
 
+    let links = '';
+
     if (user) {
+        links = `
+            <li class="link-item dynamic-auth-link"><a href="/dashboard.html" class="link">Dashboard</a></li>
+            <li class="link-item dynamic-auth-link"><a href="#" onclick="logoutUser()" class="link">Logout</a></li>
+        `;
         user.getIdTokenResult(true).then(idTokenResult => {
-            let links = `
-                <li class="link-item dynamic-auth-link"><a href="/dashboard.html" class="link">Dashboard</a></li>
-                <li class="link-item dynamic-auth-link"><a href="#" onclick="logoutUser()" class="link">Logout</a></li>
-            `;
             if (idTokenResult.claims.admin) {
-                links += `<li class="link-item dynamic-auth-link"><a href="/admin-panel" class="link" style="color: red; font-weight: bold;">Admin</a></li>`;
+                let adminLink = `<li class="link-item dynamic-auth-link"><a href="/admin-panel" class="link" style="color: red; font-weight: bold;">Admin</a></li>`;
+                ul.insertAdjacentHTML('beforeend', adminLink);
+                if(ur) ur.insertAdjacentHTML('beforeend', adminLink.replace(/link-item/g, 'links-item'));
             }
-            // Adds links WITHOUT deleting the menu button
-            ul.insertAdjacentHTML('beforeend', links);
-            if(ur) ur.innerHTML = links.replace(/link-item/g, 'links-item');
         });
     } else {
-        ul.insertAdjacentHTML('beforeend', `<li class="link-item dynamic-auth-link"><a href="/dashboard.html" class="link">Login</a></li>`);
+        links = `<li class="link-item dynamic-auth-link"><a href="/dashboard.html" class="link">Login</a></li>`;
+    }
+
+    // Add links to the main navbar
+    ul.insertAdjacentHTML('beforeend', links);
+
+    // FIX: Add links to sidebar WITHOUT deleting the 'X' button
+    if (ur) {
+        let sidebarLinks = links.replace(/link-item/g, 'links-item');
+        ur.insertAdjacentHTML('beforeend', sidebarLinks);
     }
 });
